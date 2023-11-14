d5c91221f56c1aa0bb628abc9ea248bb395422df
sobhiEgbaria <sobhim686@gmail.com>
2023-11-14 22:25:56 +0200

    finsh section 10

--- day_10/calc.py
+++ day_10/calc.py
@@ -28,16 +28,73 @@ operation = {
     "/": divide,
 }
 
-num1 = int(input("what is the first number?: "))
-num2 = int(input("what is the second number?: "))
+"""
+the full solution
+"""
 
-for i in operation:
-    print(i)
+# def calculator():
+#     keep_on = True
 
-operation_symbol = input("pick an operation symbol?: ")
+#     num1 = float(input("what is the first number?: "))
+#     for i in operation:
+#         print(i)
+#     operation_symbol = input("pick an operation symbol?: ")
+#     num2 = float(input("what is the second number?: "))
 
-fun = operation[operation_symbol]
+#     operation_function = operation[operation_symbol]
 
-res = fun(num1, num2)
+#     res = operation_function(num1, num2)
 
-print(f"{num2} {operation_symbol} {num2} = {res}")
+#     print(f"{num1} {operation_symbol} {num2} = {res}")
+
+#     while keep_on:
+#         keep_on = input(
+#             f"type 'Y' to continue calculate with {res}, or 'N' to start new calculation: "
+#         ).lower()
+#         if keep_on == "n":
+#             clear()
+#             calculator()
+#         else:
+#             operation_symbol = input("pick an operation symbol?: ")
+#             num3 = int(input("what is the new number??: "))
+#             num2 = res
+#             operation_function = operation[operation_symbol]
+#             res = operation_function(res, num3)
+#             print(f"{num2} {operation_symbol} {num3} = {res}")
+
+
+# calculator()
+
+# ## ========================================================================================
+# """
+# anther solution (shorter)
+# """
+
+
+# def calculator():
+#     keep_on = True
+
+#     num1 = float(input("what is the first number?: "))
+#     for i in operation:
+#         print(i)
+#     operation_symbol = input("pick an operation symbol?: ")
+
+#     while keep_on:
+#         num2 = float(input("what is the anther number?: "))
+#         operation_function = operation[operation_symbol]
+#         res = operation_function(num1, num2)
+
+#         print(f"{num1} {operation_symbol} {num2} = {res}")
+
+#         keep_on = input(
+#             f"type 'Y' to continue calculate with {res}, or 'N' to start new calculation: "
+#         ).lower()
+#         if keep_on == "n":
+#             clear()
+#             calculator()
+#         else:
+#             operation_symbol = input("pick an operation symbol?: ")
+#             num1 = res
+
+
+# calculator()
